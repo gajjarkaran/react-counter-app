cd0b8ca98cfeb21cecabe5b0734a4da67ed659d5
Karan Gajjar <karangajjar96@gmail.com>
2021-09-12 23:58:55 -0500

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Component } from "react";
+
+export default class Button extends Component {
+  render() {
+    return <button onClick={this.props.action}>{this.props.title}</button>;
+  }
+}
